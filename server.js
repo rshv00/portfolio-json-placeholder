19a8f5d7b2984f620cd0613fc9abd69fea7c0b96
Roman Shevchuk <rshv00@gmail.com>
2020-11-07 14:06:05 +0200

    disable cors

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const jsonServer = require('json-server');
+const cors = require('cors');
 const server = jsonServer.create();
 const port = process.env.PORT || 4000;
 
 server.use('/api', jsonServer.router('db.json'));
-
+server.use(cors());
 server.listen(port);
